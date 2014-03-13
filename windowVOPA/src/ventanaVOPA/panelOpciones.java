01387565f18e091e5b7f38d5b7242fbaaf23c2d6
Industriales <alberto.martinez06@goumh.umh.es>
2014-03-13 23:04:19 +0100

    Second commit
    Updates

--- windowVOPA/src/ventanaVOPA/panelOpciones.java
+++ windowVOPA/src/ventanaVOPA/panelOpciones.java
@@ -12,9 +12,8 @@ import javax.swing.JTextArea;
 public class panelOpciones extends JPanel{
 	
 	
-	private static JLabel ordenador, usuario;
-	public static JTextArea areaord, areausu;
-	public static JPanel panel;
+	private static JLabel ordenador, usuario, textord, textusu;
+	public static JPanel areaord, areausu;
 
 	
 	public panelOpciones(){
@@ -32,27 +31,34 @@ public class panelOpciones extends JPanel{
 				 usuario = new JLabel("Usuario");
 				 usuario.setBounds(25, 180, 150, 10);
 				 usuario.setBackground(Color.black);
-				 
-				 areaord = new JTextArea();
+				 				 
+				// Para pintar el fondo blanco igual que antes
+				 areaord = new JPanel();
 				 areaord.setBounds(25, 50, 150, 50);
 				 
-				 areausu = new JTextArea();
+				 areausu = new JPanel();
 				 areausu.setBounds(25, 200, 150, 50);
-			 
 				 
-				 //visualizar
+				// Para escribir el texto sin necesidad de usar el cursor (texto fijo)
+				 textord = new JLabel("hola mundo");
+				 textord.setBounds(28, 50, 150, 50);
 				 
-				 panelPaint p = new panelPaint();
+				 textusu = new JLabel("hola mundo");
+				 textusu.setBounds(28, 200, 150, 50);
 				 
-				 areausu.setText("hola");
-				 areaord.setText("hola");
+				 
+				// textusu.setText("Hola mundo");
+				 //textord.setText("Hola mundo");
 				 
 				 
 				//agregamos elementos al panel
 				 add(ordenador);
 				 add(usuario);
+				 add(textord);
+				 add(textusu);	
 				 add(areaord);
-				 add(areausu);			 
+			     add(areausu);;
+				
 			
 	
 	}
--- windowVOPA/src/ventanaVOPA/panelPaint.java
+++ windowVOPA/src/ventanaVOPA/panelPaint.java
@@ -5,12 +5,16 @@ import java.awt.*;
 import javax.swing.JPanel;
 import javax.swing.JButton;
 
+
+
 import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
+import java.awt.Image;
 import java.awt.KeyEventDispatcher;
 import java.awt.event.*;
 import java.awt.geom.*;
+import java.awt.image.BufferedImage;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -18,26 +22,31 @@ import java.util.StringTokenizer;
 
 
 public class panelPaint extends JPanel implements KeyListener {
-//Se g
-	//public static JPanel panel2;
+
+	//declaracion de las variables
 	public static JButton boton;
 	public static int n=0;
-	public static int buffer=0;//PRUEBO A PONER BUFFER=1 , PRUEBAAAAAAAAAAAAAAAAAA
-	int x=0, y=0, xx=-1, yy=-1,ab=0,ar=0,izq=0,der=0;
+	public static int buffer;
+	int x=-2, y=-2, xx=-1, yy=-1,ab=0,ar=0,izq=0,der=0;
 	public static Graphics2D g;
 	public String l;
-	boolean dibuja=false;
-	int matriz[][]=new int[300][2];//NUEVO
+	boolean dibuja = false, zoom = false;
+	int[][] matriz=new int[300][2];//NUEVO//declaramos una matriz para almacenar nuestro dibujo
 	int v[]=new int[2];//NUEVO
-	//int a=-1,b=-1;
-	//Graphics gBuffer;//prueba rara
+	int ancho=20, largo=20, mover=20, zo=0, variable =0;
+	float q=1;
+	//String color = "black";
+	
+	
+	
+	
+	
+	
+	
 	public panelPaint(){
 		n=buffer;
-		
-		
-		//initComponent();
-		boton = new JButton();
-		boton.setBounds(x, y, 20, 20);
+		boton = new JButton();//llamamos al boton
+		boton.setBounds(x, y, ancho, largo);
 	
 		setLayout(null);
 		setBounds(201,0,1400,800);
@@ -46,41 +55,68 @@ public class panelPaint extends JPanel implements KeyListener {
 		boton.addKeyListener(this);
 		
 	}
-
+///////////////////////////////////////////////////////////////////
+	public void zoom(){
+		
+		if(zoom){
+		ancho = (int) (ancho*q);	// se multiplica para darle proporcion a las dimensiones y el movimiento del puntero
+		largo = (int) (largo*q);
+		mover = (int) (mover*q);
+		
+		y = (int) (y*q); // calibrar el puntero
+		x = (int) (y*q);
+		
+		int j;
+		for(j=0;j<300;j++){
+			
+			matriz[j][0] = (int) (matriz[j][0]*q);
+			matriz[j][1] = (int) (matriz[j][1]*q);
+			repaint();
+			dibuja = true;
+			
+			zo=1;
+			
+		}
+		
+		System.out.println("zoom");
+		
+		}
+	}
+	
 
 /////////////////metodos con el teclado////////////////////////////
 	@Override
 	public void keyPressed(KeyEvent e) {
 		int j,k,m;//NUEVO
-		//System.out.println(e.getKeyText(e.getKeyCode()));
-		System.out.println("POSICION ACTUAL PRIMERO "+xx+"," +yy);//PRUEBA PARA COMPROBAR POSICION
-		for(m=0;m<buffer;m++)
+		
+		for(m=0;m<=buffer;m++)
 		{
 			System.out.println("elemento "+m);
 			System.out.print(matriz[m][0]+" ,"+matriz[m][1]+"\n");
 		}
-		//System.out.println("VARIABLES a y b "+a +b);
+		
 		
 		l = e.getKeyText(e.getKeyCode());
+
 			if(l.equals("Arriba")){
 
-			y-=20;
-			//boton.setBounds(x, y, 20, 20);
+			y-=mover;
 			v[0]=boton.getX();
 			v[1]=boton.getY();
 			ar=1;
-			//dibuja=true;
 			
-			for(j=0;j<buffer;j++)
+			
+			for(j=0;j<=buffer;j++)
 			{
 				if(matriz[j][0]==(v[0]))
 				{
 					if(matriz[j][1]==(v[1]))
 					{
 						xx = boton.getX();
-						yy = boton.getY();
+						yy = boton.getY();//HE AÑADIDO EL 20, VEREMOS A VER
 						System.out.println("he pulsado arriba y tiene que redibujar");
 						dibuja=true;
+						
 					}
 				}
 			}
@@ -90,23 +126,22 @@ public class panelPaint extends JPanel implements KeyListener {
 
 			else if(l.equals("Abajo")){
 
-			y+=20;
-			//boton.setBounds(x, y, 20, 20);
+			y+=mover;
 			v[0]=boton.getX();
 			v[1]=boton.getY();
 			ab=1;
-			//dibuja=true;
-			
-			for(j=0;j<buffer;j++)
+						
+			for(j=0;j<=buffer;j++)
 			{
 				if(matriz[j][0]==(v[0]))
 				{
 					if(matriz[j][1]==(v[1]))
 					{
 						xx = boton.getX();
-						yy = boton.getY();
+						yy = boton.getY();//HE AÑADIDO EL 20, VEREMOS A VER
 						System.out.println("he pulsado abajo y tiene que redibujar");
 						dibuja=true;
+						
 					}
 				}
 			}
@@ -114,14 +149,12 @@ public class panelPaint extends JPanel implements KeyListener {
 
 			else if(l.equals("Derecha")){
 
-			x+=20;
-			//boton.setBounds(x, y, 20, 20);
+			x+=mover;
 			v[0]=boton.getX();
 			v[1]=boton.getY();
 			der=1;
-			//dibuja=true;
 			
-			for(j=0;j<buffer;j++)
+			for(j=0;j<=buffer;j++)
 			{
 				if(matriz[j][0]==(v[0]))
 				{
@@ -131,6 +164,7 @@ public class panelPaint extends JPanel implements KeyListener {
 						yy = boton.getY();
 						System.out.println("he pulsado derecha y tiene que redibujar");
 						dibuja=true;
+						
 					}
 				}
 			}
@@ -139,14 +173,12 @@ public class panelPaint extends JPanel implements KeyListener {
 
 			else if(l.equals("Izquierda")){
 
-			x-=20;
-			//boton.setBounds(x, y, 20, 20);
+			x-=mover;
 			v[0]=boton.getX();
 			v[1]=boton.getY();
 			izq=1;
-			//dibuja=true;
 			
-			for(j=0;j<buffer;j++)
+			for(j=0;j<=buffer;j++)
 			{
 				if(matriz[j][0]==(v[0]))
 				{
@@ -156,6 +188,7 @@ public class panelPaint extends JPanel implements KeyListener {
 						yy = boton.getY();
 						System.out.println("he pulsado izquierda y tiene que redibujar");
 						dibuja=true;
+						
 					}
 				}
 			}
@@ -166,41 +199,60 @@ public class panelPaint extends JPanel implements KeyListener {
 				System.out.println("HE PULSADO A \n");//NUEVO
 				xx = boton.getX();
 				yy = boton.getY();
-				//a=a*(-1);
-				System.out.println("He entrado a A");
-				//b=b*(-1);
-				//System.out.println("el valor de buffer ANTES es");//NUEVO
-				//System.out.println(buffer);//NUEVO
 				matriz[buffer][0]=xx;//NUEVO
 				matriz[buffer][1]=yy;//NUEVO
-				//System.out.println(matriz[buffer][0]);//NUEVO
-				//System.out.println(matriz[buffer][1]);//NUEVO
 				n=1+n;//NUEVO
 				buffer=n;
-				//System.out.println("el valor de buffer DESPUES es");//NUEVO
-				//System.out.println(buffer);//NUEVO
 				dibuja=true;
-				//paintComponent(g);
+
 
 			}
+			
+			else if(l.equals("Q")){
+				System.out.println("HE PULSADO Q \n");// Llama al metodo zoom y multiplica la constante q
+				q=(float) 0.5;
+				zoom=true;
+				zoom();			
+			}
+			
+			else if(l.equals("W")){
+				System.out.println("HE PULSADO w \n");
+				q=2;
+				zoom=true;
+				zoom();			
+			}
+			
+			else if(l.equals("B")){
+				System.out.println("HE PULSADO B \n");
+				xx = boton.getX();
+				yy = boton.getY();
+			/*	matriz[buffer][0]=xx;//NUEVO intento que se conserve el borrado!!!!
+				matriz[buffer][1]=yy;//NUEVO
+				n=1+n;//NUEVO
+				buffer=n; */
+				variable=1;
+				dibuja=true;
+			}
+			
+		
 			if(ar==1)
 			{
-				boton.setBounds(x, y, 20, 20);
+				boton.setBounds(x, y, ancho, largo);
 				ar=0;
 			}
 			if(ab==1)
 			{
-				boton.setBounds(x, y, 20, 20);
+				boton.setBounds(x, y, ancho, largo);
 				ab=0;
 			}
 			if(izq==1)
 			{
-				boton.setBounds(x, y, 20, 20);
+				boton.setBounds(x, y, ancho, largo);
 				izq=0;
 			}
 			if(der==1)
 			{
-				boton.setBounds(x, y, 20, 20);
+				boton.setBounds(x, y, ancho, largo);
 				der=0;
 			}
 
@@ -208,29 +260,47 @@ public class panelPaint extends JPanel implements KeyListener {
 		
 	}
 	public void paintComponent(Graphics g){
-		//pienso que falla porque al coger xx e yy valores, luego siempre entra
-		//e intenta pintar de negro
-				//gBuffer.setColor(Color.red);//prueba rara
-		System.out.println("POSICION ACTUAL SEGUNDA "+xx+"," +yy);
+		
 				super.paintComponent(g);
 				g = (Graphics2D) g;
-				//System.out.println("voy a ver si pinto esta posicion:"+xx);//NUEVO
-				//System.out.println("voy a ver si pinto esta otra posicion"+yy);//NUEVO
+				
+				if(zo==1){
+					
+					g.setColor(Color.black);
+					if(dibuja){
+					for(int j=0;j<300;j++){
+						
+						xx = matriz[j][0];
+						yy = matriz[j][1];
+						boton.setBounds(x, y, ancho, largo);				
+						g.fillRect(xx,yy, ancho, largo);
+						xx=-1;
+						yy=-1;
+						}	
+					zo=0;
+						}	
+				}
+
 				if (xx != -1 && yy != -1){
 				System.out.println("pinto");
+				
 				g.setColor(Color.black);
-				if (dibuja)
-					{
-					//System.out.println("seguro"+xx);//NUEVO
-					//System.out.println("seguro segundo"+yy);//NUEVO
-					
-					g.fillRect(xx,yy,20,20);
-
-					xx=-1;
-					yy=-1;
+					if (dibuja)
+						{
+							if(variable==1){
+								g.setColor(Color.white);
+								g.fillRect(xx,yy, ancho, largo);
+								variable=0;
+								
+							}
+						
+						g.fillRect(xx,yy, ancho, largo);
+						
+						xx=-1;
+						yy=-1;		
+						}
+				
 					}
-					
-				}
 				
 			}
 	
